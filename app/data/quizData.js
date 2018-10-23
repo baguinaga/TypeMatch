157d781831b8b210dc71bafc7200bea1ed8b486f
baguinaga <42448094+Baguinaga@users.noreply.github.com>
2018-10-22 22:59:35 -0400

    added more seed data

--- app/data/quizData.js
+++ app/data/quizData.js
@@ -1,8 +1,49 @@
+//E: 40% , I: 60% : S: 30%, N: 70% : T:65%, F: 35% : J: 70%, P: 30%
 const quizData = [{
   name: "Billy Aguinaga",
-  url: "placeholder",
+  url: "/assets/images/notReallyMe.jpg",
   score: [1, 4, 1, 4, 5, 0, 4, 1],
   type: "INTJ"
+}, {
+  name: "Queen Elizabeth I",
+  url: "https://www.rmg.co.uk/sites/default/files/Armada%20Portrait%20thumbnail.jpg",
+  score: [4, 1, 2, 3, 5, 0, 4, 1],
+  type: "ENTJ"
+}, {
+  name: "Edgar Allen Poe",
+  url: "https://ffrf.org/media/k2/items/cache/b917d7b35b000717a1e15f413b813b33_XL.jpg",
+  score: [1, 4, 0, 5, 1, 4, 2, 3],
+  type: "INFP"
+}, {
+  name: "Malcolm X",
+  url: "https://upload.wikimedia.org/wikipedia/commons/c/cb/Malcolm_X_NYWTS_2a.jpg",
+  score: [4, 1, 3, 2, 5, 0, 2, 3],
+  type: "ESTP"
+}, {
+  name: "Vladimir Lenin",
+  url: "https://www.biography.com/.image/t_share/MTIwNjA4NjMzODgyNTEwODYw/vladimir-lenin-9379007-1-402.jpg",
+  score: [2, 3, 0, 5, 3, 2, 4, 1],
+  type: "INTJ"
+}, {
+  name: "Pablo Picasso",
+  url: "https://news.artnet.com/app/news-upload/2015/07/5886813703_01fa96bfc2_b-803x1024.jpg",
+  score: [1, 4, 3, 2, 1, 4, 2, 3],
+  type: "ISFP"
+}, {
+  name: "Rose Parks",
+  url: "https://www.biography.com/.image/t_share/MTE1ODA0OTcxNzQ5Mzc3NTQ5/rosa-parks-9433715-1-402.jpg",
+  score: [2, 3, 4, 1, 0, 5, 4, 1],
+  type: "ISFJ"
+}, {
+  name: "Theodore Roosevelt",
+  url: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/President_Roosevelt_-_Pach_Bros.jpg/1200px-President_Roosevelt_-_Pach_Bros.jpg",
+  score: [5, 0, 2, 3, 4, 1, 2, 3],
+  type: "ENTP"
+}, {
+  name: "Ernest Hemingway",
+  url: "https://upload.wikimedia.org/wikipedia/commons/2/28/ErnestHemingway.jpg",
+  score: [2, 3, 3, 2, 3, 2, 1, 4],
+  type: "ISTP"
 }];
 
 module.exports = quizData;
